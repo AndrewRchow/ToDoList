624cd7d02e13307d21817a21bf61d94bf3c9e21c
Andrew Chow <chowlick@gmail.com>
2017-09-11 11:53:31 -0700

    added web

---
+++ web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToDoList.web.Startup))]
+namespace ToDoList.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
